e39c2e628cbe5902efe75e73403c4403610b77e5
moshenglei <moshenglei@icloud.com>
2019-10-11 00:00:55 +0800

    init

---
+++ lanmao-user-bops/src/main/java/com/lanmao/user/LanmaoUserBopsApplication.java
@@ -0,0 +1,13 @@
+package com.lanmao.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanmaoUserBopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanmaoUserBopsApplication.class, args);
+	}
+
+}
