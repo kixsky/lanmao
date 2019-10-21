cc07341f58963c63266e22e1dcf73ba8a10a188c
moshenglei <moshenglei@icloud.com>
2019-10-21 22:22:12 +0800

    feign

--- lanmao-user-bops/src/main/java/com/lanmao/user/LanmaoUserBopsApplication.java
+++ lanmao-user-bops/src/main/java/com/lanmao/user/LanmaoUserBopsApplication.java
@@ -3,11 +3,13 @@ package com.lanmao.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
+@EnableFeignClients(basePackages = "com.lanmao.core.share")
 public class LanmaoUserBopsApplication {
 
 	public static void main(String[] args) {
