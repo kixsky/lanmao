d2dbc947e6fb963f2f5fd3b5a6676a6a4c5cdd7e
moshenglei <moshenglei@icloud.com>
2019-10-21 23:09:11 +0800

    commit

--- lanmao-user-bops/src/main/java/com/lanmao/user/controller/SmsController.java
+++ lanmao-user-bops/src/main/java/com/lanmao/user/controller/SmsController.java
@@ -2,6 +2,7 @@ package com.lanmao.user.controller;
 
 import com.lanmao.common.annotation.IgnorePath;
 import com.lanmao.common.bean.BaseResult;
+import com.lanmao.common.constants.ErrorCodeEnum;
 import com.lanmao.core.share.service.SmsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,14 @@ public class SmsController {
     @IgnorePath
     @RequestMapping(value = "/sendCode", method = RequestMethod.POST)
     public BaseResult<String> sendCode(@RequestParam("mobile") String mobile) {
-        return smsService.sendCode(mobile);
+        BaseResult<String> baseResult = new BaseResult<>();
+        try {
+            baseResult = smsService.sendCode(mobile);
+        } catch (Exception e) {
+            log.info("sendCode", e);
+            baseResult.setCode(ErrorCodeEnum.CODE_FAIL.getCode());
+            baseResult.setMessage(e.getMessage());
+        }
+        return baseResult;
     }
 }
