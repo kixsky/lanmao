31c8002474e80146d295acfa2a240fbc0cc5de06
moshenglei <moshenglei@icloud.com>
2019-10-27 22:56:11 +0800

    feign

--- lanmao-mech-bops/src/main/java/com/lanmao/mech/LanmaoMechBopsApplication.java
+++ lanmao-mech-bops/src/main/java/com/lanmao/mech/LanmaoMechBopsApplication.java
@@ -3,9 +3,12 @@ package com.lanmao.mech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.lanmao.core.share")
 public class LanmaoMechBopsApplication {
 
 	public static void main(String[] args) {
